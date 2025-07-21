b3cba11a58c5ae5800002fc69375143f9c688ae6
Mihirs_Legion <mihirmainkarm3@gmail.com>
2025-07-21 13:14:59 +0100

    test2

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const startCam = async () => {
 };
 startCam();
 
-// Three.js
+// Three.js setup
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, 640/480, 0.01, 10);
+const camera = new THREE.PerspectiveCamera(70, 640 / 480, 0.01, 10);
 camera.position.z = 1;
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(640, 480);
@@ -33,18 +33,20 @@ scene.add(shieldMesh);
 // MediaPipe setup
 let lastFace = null, lastHand = null;
 
-const faceMesh = new FaceMesh.FaceMesh({
+// Updated constructors!
+const faceMesh = new FaceMesh({
   locateFile: f => `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${f}`
 });
 faceMesh.setOptions({ maxNumFaces: 1, refineLandmarks: true });
 faceMesh.onResults(res => lastFace = res.multiFaceLandmarks?.[0] || null);
 
-const hands = new Hands.Hands({
+const hands = new Hands({
   locateFile: f => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${f}`
 });
 hands.setOptions({ maxNumHands: 1 });
 hands.onResults(res => lastHand = res.multiHandLandmarks?.[0] || null);
 
+// Frame processing
 async function process() {
   await faceMesh.send({ image: video });
   await hands.send({ image: video });
@@ -58,13 +60,17 @@ function animate() {
     const pt = lastFace[1];
     maskMesh.position.set(pt.x - 0.5, -(pt.y - 0.5), -pt.z);
     maskMesh.visible = true;
-  } else maskMesh.visible = false;
+  } else {
+    maskMesh.visible = false;
+  }
 
   if (lastHand) {
     const pt = lastHand[0];
     shieldMesh.position.set(pt.x - 0.5, -(pt.y - 0.5), -pt.z);
     shieldMesh.visible = true;
-  } else shieldMesh.visible = false;
+  } else {
+    shieldMesh.visible = false;
+  }
 
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
